a1e340904a7a3f3dd942e58488682edd2fb13769
sisheng <2843643165@qq.com>
2023-02-17 22:38:46 +0800

    添加第三方网络请求包的依赖

--- main.js
+++ main.js
@@ -2,6 +2,21 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
