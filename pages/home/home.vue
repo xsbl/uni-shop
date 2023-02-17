e9e32ce3d9b941692f009afc467de5371a3ea7ca
sisheng <2843643165@qq.com>
2023-02-17 23:01:27 +0800

    请求轮播图数据

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,8 +8,28 @@
 	export default {
 		data() {
 			return {
-				
+				// 这是轮播图的数据列表
+				swiperList: [],
 			};
+		},
+		onLoad() {
+			console.log('hhhhhhhhhh')
+		      // 调用方法，获取轮播图的数据
+		      this.getSwiperList()
+		},
+		methods: {
+		  async getSwiperList() {
+			const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+			// 请求失败
+			if (res.meta.status !== 200) {
+				return uni.showToast({
+				  title: '数据请求失败！',
+				  duration: 1500,
+				  icon: 'none',
+				})
+			}
+			this.swiperList = res.message
+		  },
 		}
 	}
 </script>
